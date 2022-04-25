541522d9522665b69abcca0de3faee62a86e7a88
degen-dev <immanuel.lam@education.nsw.gov.au>
2022-04-25 18:37:37 +1000

    sauce?

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
     themeConfig: {
       logo: '/assets/img/logo.png',
+      themeConfig: {
+        sidebar: [
+          '/',
+          '/page-a',
+          ['/page-b', 'Explicit link text']
+        ]
+      }
     },
     title: 'Inanis Dev Team',
     description: 'Official Documentation'
