4400d53bfefb102cf5bbccadb5472fadf4cc0ce4
degen-dev <immanuel.lam@education.nsw.gov.au>
2022-04-26 11:59:02 +1000

    actually have the right repo

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,21 +3,11 @@ module.exports = {
     logo: "/assets/img/logo.png",
     lastUpdated: "Last Updated", // string | boolean
     repo: "InanisDev/docs",
-    // Customising the header label
-    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: "Contribute!",
-
-    // Optional options for generating "Edit this page" link
-
-    // if your docs are in a different repo from your main project:
-    docsRepo: "vuejs/vuepress",
-    // if your docs are not at the root of the repo:
+    docsRepo: "InanisDev/docs",
     docsDir: "docs",
-    // if your docs are in a specific branch (defaults to 'master'):
-    docsBranch: "master",
-    // defaults to false, set to true to enable
+    docsBranch: "dev",
     editLinks: true,
-    // custom text for edit link. Defaults to "Edit this page"
     editLinkText: "Help us improve this page!",
     nav: [
       { text: "Home", link: "/" },
